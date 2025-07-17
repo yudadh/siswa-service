845eea38a9fc7e081e559990bfadf3d99c4671ee
YudaDh <yudadarmawan26@gmail.com>
2025-07-18 00:31:54 +0800

    modify cors

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import morgan from "morgan";
 const app = express();
 
 const corsOptions = {
-   origin: 'http://localhost:5173',
+   origin: process.env.FRONTEND_URL,
    credentials: true
 }
 app.use(cors(corsOptions));
